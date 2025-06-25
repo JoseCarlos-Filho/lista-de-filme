d5d03c17a3283114c580f148eeb06c592c2e60a8
JoseCarlos-Filho <etijcvivi@gmail.com>
2025-06-24 22:10:04 -0300

    feat: adiciona validação de nome e gênero, e previne duplicação de filmes

--- js/main.js
+++ js/main.js
@@ -1,14 +1,39 @@
 import ui from "./ui.js"
 import api from "./api.js"
 
-const regexConteudo = /^[A-Za-z\s]{10,}$/
+const filmesSet = new Set();
 
-function validarConteudo(conteudo) {
-  return regexConteudo.test(conteudo)
+async function adicionarChaveAoFilme() {
+  try {
+    const filmes = await api.buscarFilmes();
+    filmes.forEach(filme => {
+      const chaveFilme = 
+      `${filme.nome.trim().toLowerCase()}-${filme.genero.trim().toLowerCase()}`;
+      filmesSet.add(chaveFilme);
+    });
+  } catch (error) {
+    alert("Erro ao adicionar chave ao filme");
+  }
+}
+
+const regexNome = /^[A-Za-z0-9\s]+$/
+const regexGenero = /^[\p{L}\s]+$/u
+
+function validarNome(Nome) {
+  return regexNome.test(Nome)
+}
+
+function validarGenero(genero) {
+  return regexGenero.test(genero)
+}
+
+function removerEspacos(string) {
+    return string.replaceAll(/\s+/g, '')
 }
 
 document.addEventListener("DOMContentLoaded", () => {
   ui.renderizarFilmes()
+  adicionarChaveAoFilme()
 
   const formularioFilme = document.getElementById("filme-form")
   const botaoCancelar = document.getElementById("botao-cancelar")
@@ -26,15 +51,35 @@ async function manipularSubmissaoFormulario(event) {
   const genero = document.getElementById("filme-genero").value
   const data = document.getElementById("filme-data").value
 
-  if (!validarConteudo(nome) || !validarConteudo(genero)) {
-    alert("Nome e gênero devem conter pelo menos 10 caracteres alfabéticos.");
+  const nomeSemEspacos = removerEspacos(nome)
+  const generoSemEspacos = removerEspacos(genero)
+
+  if (!nomeSemEspacos || !generoSemEspacos || !data) {
+    alert("Por favor, preencha todos os campos.");
+    return;
+  }
+
+  if (!validarNome(nomeSemEspacos)) {
+    alert("Nome inválido! O nome deve conter apenas letras, números e espaços.");
     nome.value = "";
+    return;
+  }
+
+  if (!validarGenero(generoSemEspacos)) {
+    alert("Gênero inválido! O gênero deve conter apenas letras.");
     genero.value = "";
     return;
   }
 
   if (!validarData(data)) {
     alert("Data inválida! A data deve ser igual ou anterior à data atual.");
+    return;
+  }
+
+  const chaveNovoFilme = `${nome.trim().toLowerCase()}-${genero.trim().toLowerCase()}`;
+
+  if(filmesSet.has(chaveNovoFilme)) {
+    alert("Filme já cadastrado!");
     return;
   }
 
