bf8f2f69a961c4065941518e441fc3b6ec7411f5
JoseCarlos-Filho <etijcvivi@gmail.com>
2025-06-30 22:57:34 -0300

    refactor: melhora a formatação e a estrutura do código no módulo ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,56 +1,57 @@
-import api from "./api.js"
+import api from "./api.js";
 
 const ui = {
-
   async preencherFormulario(filmeId) {
-    const filme = await api.buscarFilmePorId(filmeId)
-    document.getElementById("filme-id").value = filme.id
-    document.getElementById("filme-nome").value = filme.nome
-    document.getElementById("filme-genero").value = filme.genero
-    document.getElementById("filme-data").value = filme.data.toISOString().split("T")[0]
+    const filme = await api.buscarFilmePorId(filmeId);
+    document.getElementById("filme-id").value = filme.id;
+    document.getElementById("filme-nome").value = filme.nome;
+    document.getElementById("filme-genero").value = filme.genero;
+    document.getElementById("filme-data").value = filme.data
+      .toISOString()
+      .split("T")[0];
     //document.getElementById("filme-form").scrollIntoView();
   },
 
   limparFormulario() {
-    document.getElementById("filme-form").reset()
+    document.getElementById("filme-form").reset();
   },
 
   async renderizarFilmes(filmesFiltrados) {
-    const listaFilmes = document.getElementById("lista-filmes")
-    listaFilmes.innerHTML = ""
-  
+    const listaFilmes = document.getElementById("lista-filmes");
+    listaFilmes.innerHTML = "";
+
     try {
-      let filmesParaRenderizar
+      let filmesParaRenderizar;
 
       if (filmesFiltrados) {
-        filmesParaRenderizar = filmesFiltrados
+        filmesParaRenderizar = filmesFiltrados;
       } else {
-        filmesParaRenderizar = await api.buscarFilmes()
+        filmesParaRenderizar = await api.buscarFilmes();
       }
-      
-      filmesParaRenderizar.forEach(ui.adicionarFilmeNaLista)
+
+      filmesParaRenderizar.forEach(ui.adicionarFilmeNaLista);
     } catch {
-      alert('Erro ao renderizar filmes')
+      alert("Erro ao renderizar filmes");
     }
   },
 
   adicionarFilmeNaLista(filme) {
-    const listaFilmes = document.getElementById("lista-filmes")
-    const li = document.createElement("li")
-    li.setAttribute("data-id", filme.id)
-    li.classList.add("li-filme")
+    const listaFilmes = document.getElementById("lista-filmes");
+    const li = document.createElement("li");
+    li.setAttribute("data-id", filme.id);
+    li.classList.add("li-filme");
 
-    const filmeNome = document.createElement("div")
-    filmeNome.textContent = filme.nome
-    filmeNome.classList.add("filme-nome")
+    const filmeNome = document.createElement("div");
+    filmeNome.textContent = filme.nome;
+    filmeNome.classList.add("filme-nome");
 
-    const filmeGenero = document.createElement("div")
-    filmeGenero.textContent = filme.genero
-    filmeGenero.classList.add("filme-genero")
+    const filmeGenero = document.createElement("div");
+    filmeGenero.textContent = filme.genero;
+    filmeGenero.classList.add("filme-genero");
 
-    const filmeDataLabel = document.createElement("label")
-    filmeDataLabel.textContent = "Data de Lançamento:"
-    filmeDataLabel.classList.add("filme-data-label")
+    const filmeDataLabel = document.createElement("label");
+    filmeDataLabel.textContent = "Data de Lançamento:";
+    filmeDataLabel.classList.add("filme-data-label");
 
     const filmeData = document.createElement("div");
     const dataFormatada = filme.data.toLocaleDateString("pt-BR", {
@@ -58,72 +59,81 @@ const ui = {
       year: "numeric",
       // month: "long",
       // day: "numeric",
-      timeZone: "UTC"
+      timeZone: "UTC",
     });
     filmeData.textContent = dataFormatada;
     filmeData.classList.add("filme-data");
 
-    const botaoEditar = document.createElement("button")
-    botaoEditar.classList.add("botao-editar")
-    botaoEditar.onclick = () => ui.preencherFormulario(filme.id)
+    const cardFilme = document.createElement("div");
+    cardFilme.classList.add("card-filme");
+    const cardFilmeImagem = document.createElement("img");
+    cardFilmeImagem.classList.add("card-filme-imagem");
+    cardFilmeImagem.src = filme.imageUrl;
+    cardFilmeImagem.alt = filme.nome;
 
-    const iconeEditar = document.createElement("img")
-    iconeEditar.src = "assets/imagens/icone-editar.png"
-    iconeEditar.alt = "Editar"
-    botaoEditar.appendChild(iconeEditar)
+    const botaoEditar = document.createElement("button");
+    botaoEditar.classList.add("botao-editar");
+    botaoEditar.onclick = () => ui.preencherFormulario(filme.id);
 
-    const botaoExcluir = document.createElement("button")
-    botaoExcluir.classList.add("botao-excluir")
+    const iconeEditar = document.createElement("img");
+    iconeEditar.src = "assets/imagens/icone-editar.png";
+    iconeEditar.alt = "Editar";
+    botaoEditar.appendChild(iconeEditar);
+
+    const botaoExcluir = document.createElement("button");
+    botaoExcluir.classList.add("botao-excluir");
     botaoExcluir.onclick = async () => {
       try {
-        await api.excluirFilme(filme.id)
-        ui.renderizarFilmes()
+        await api.excluirFilme(filme.id);
+        ui.renderizarFilmes();
       } catch (error) {
-        alert("Erro ao excluir filme")
-        throw error
+        alert("Erro ao excluir filme");
+        throw error;
       }
-    }
+    };
 
-    const iconeExcluir = document.createElement("img")
-    iconeExcluir.src = "assets/imagens/icone-excluir.png"
-    iconeExcluir.alt = "Excluir"
-    botaoExcluir.appendChild(iconeExcluir)
+    const iconeExcluir = document.createElement("img");
+    iconeExcluir.src = "assets/imagens/icone-excluir.png";
+    iconeExcluir.alt = "Excluir";
+    botaoExcluir.appendChild(iconeExcluir);
 
-    const botaoFavorito = document.createElement("button")
+    const botaoFavorito = document.createElement("button");
     botaoFavorito.classList.add("botao-favorito");
     botaoFavorito.onclick = async () => {
       try {
-        filme.favorito = !filme.favorito
-        await api.atualizarFavorito(filme.id, filme.favorito)
-        ui.renderizarFilmes()
+        filme.favorito = !filme.favorito;
+        await api.atualizarFavorito(filme.id, filme.favorito);
+        ui.renderizarFilmes();
       } catch (error) {
-        alert("Erro ao atualizar favorito")
-        throw error
+        alert("Erro ao atualizar favorito");
+        throw error;
       }
-    }
-    
-    const iconeFavorito = document.createElement("img")
-    iconeFavorito.src = filme.favorito ?
-      "assets/imagens/icone-favorito.png"
-      : "assets/imagens/icone-favorito_outline.png"
+    };
 
-    iconeFavorito.alt = "icone de favorito"
-    botaoFavorito.appendChild(iconeFavorito)
+    const iconeFavorito = document.createElement("img");
+    iconeFavorito.src = filme.favorito
+      ? "assets/imagens/icone-favorito.png"
+      : "assets/imagens/icone-favorito_outline.png";
 
-    const icones = document.createElement("div")
-    icones.classList.add("icones")
-    icones.appendChild(botaoEditar)
-    icones.appendChild(botaoExcluir)
-    icones.appendChild(botaoFavorito)
+    iconeFavorito.alt = "icone de favorito";
+    botaoFavorito.appendChild(iconeFavorito);
 
-    li.appendChild(filmeNome)
-    li.appendChild(filmeGenero)
-    li.appendChild(filmeDataLabel)
-    li.appendChild(filmeData)
+    const icones = document.createElement("div");
+    icones.classList.add("icones");
+    icones.appendChild(botaoEditar);
+    icones.appendChild(botaoExcluir);
+    icones.appendChild(botaoFavorito);
 
-    li.appendChild(icones)
-    listaFilmes.appendChild(li)
-  }
-}
+    li.appendChild(filmeNome);
+    li.appendChild(filmeGenero);
+    li.appendChild(filmeDataLabel);
+    li.appendChild(filmeData);
+    li.appendChild(cardFilme);
+    cardFilme.appendChild(cardFilmeImagem);
 
-export default ui
+    li.appendChild(icones);
+    listaFilmes.appendChild(li);
+  },
+};
+
+export default ui;
